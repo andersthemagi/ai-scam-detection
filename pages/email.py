490758e811ea990017f10715ff9aaf2cf9c221f8
Eloise <145066941+CodingEloise@users.noreply.github.com>
2024-09-01 17:36:25 +0100

    Feature: added Email.py

--- pages/email.py
+++ pages/email.py
@@ -1,6 +1,41 @@
-import streamlit as st 
-from menu import menu_with_redirect 
+from openai import OpenAI
+import streamlit as st
+from menu import menu_with_redirect
 
 menu_with_redirect()
 
 st.title("Email Example")
+
+st.text("--Consistently monitoring emails and scanning possible scams...--")
+
+st.text("--Received an email from 12345@gmail.com, analysing--")
+
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o-mini"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
